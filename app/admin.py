a8ec5c18eec5b85bb79e1e92d28c7792147b554b
wagnerrafa <wagner_rafa@hotmail.com.br>
2019-07-24 11:41:06 -0300

    sqlite

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,3 @@
 from django.contrib import admin
-
+from models import Pessoa
 # Register your models here.
--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Pessoa(models.Model):
+
+    nome = models.CharField(max_length=255)
+    idade = models.IntegerField()
+
+    def __init__(self,nome,idade):
+        self.nome = nome
+        self.idade = idade
